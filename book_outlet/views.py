a1669fe80d8df5069e81bcfb6515870b3f9714c8
Bhavesh <bhaveshdaswani93@gmail.com>
2025-02-11 13:37:03 +0530

    working om book list and book detail page

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+def index(request):
+  books = Book.objects.all()
+  
+  return render('book_outlet/index.html', {
+    'books': books
+  })
+  
+def book_detail(request, id):
+  book = Book.objects.get(id=id)
+  
+  return render('book_outlet/book.html', {
+    'book': book
+  })
