7e455b75df2bb703b994c37df508721007a35241
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2025-02-13 13:47:47 +0530

    Enable slug field editing in admin, add prepopulated slug from title, and enhance book list display with filters

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -9,14 +9,14 @@ class Book(models.Model):
   rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
   author = models.CharField(max_length=100, null=True)
   is_bestselling = models.BooleanField(default=False)
-  slug = models.SlugField(default='', blank=True, editable=False, null=False, db_index=True)
+  slug = models.SlugField(default='', blank=True, null=False, db_index=True)
 
   def get_absolute_url(self):
       return reverse("book-detail", kwargs={"slug": self.slug})
       
-  def save(self, *args, **kwargs):
-    self.slug = slugify(self.title)
-    super().save(*args, **kwargs)
+  # def save(self, *args, **kwargs):
+  #   self.slug = slugify(self.title)
+  #   super().save(*args, **kwargs)
   
   
   def __str__(self):
